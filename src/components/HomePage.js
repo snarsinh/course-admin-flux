bbdb99433feaea8b63d67556b97ac7d3a41ef90e
Shams Narsinh <shams.aryans@gmail.com>
2019-09-04 20:48:43 -0700

    React components for Program Administration

---
+++ src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div className="jumbotron">
+      <h1>Program Administration</h1>
+      <p>React, Flux, and React Router based responsive web app.</p>
+    </div>
+  );
+}
+
+export default HomePage;
